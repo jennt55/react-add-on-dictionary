d930638f5157cd382da1221ed7c27958fd1cdfc5
jennt55 <146074617+jennt55@users.noreply.github.com>
2023-12-07 11:45:43 +0000

    api added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiKey = "331fct734c14fe46300adod3e6c6aacb";
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   return (
     <div className="Dictionary">
