40ef49a65de2629b64f4538b7c626477d456076a
jennt55 <146074617+jennt55@users.noreply.github.com>
2023-12-07 10:28:11 +0000

    header added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/106/291/original/open-book-outline-doodle-illustration-in-engraving-style-concept-back-to-school-vector.jpg?1701944060" />
+        </div>
+        <h1>Dictionary App</h1>
       </header>
     </div>
   );
 }
-
-export default App;
