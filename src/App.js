c1bf7d6963bf9bf4c2789c9cb0df53e39f5be193
jennt55 <146074617+jennt55@users.noreply.github.com>
2023-12-07 10:32:09 +0000

    added alt to img

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <div>
-          <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/106/291/original/open-book-outline-doodle-illustration-in-engraving-style-concept-back-to-school-vector.jpg?1701944060" />
+          <img
+            alt="open book"
+            src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/106/291/original/open-book-outline-doodle-illustration-in-engraving-style-concept-back-to-school-vector.jpg?1701944060"
+          />
         </div>
         <h1>Dictionary App</h1>
       </header>
