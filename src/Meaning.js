2920f03515bcb17acda93b7c54721fe668fefac9
jennt55 <146074617+jennt55@users.noreply.github.com>
2023-12-08 10:38:36 +0000

    added meanings, part of speech and example

---
+++ src/Meaning.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <em>{props.meaning.example}</em>
+      <p>{props.meaning.definition}</p>
+    </div>
+  );
+}
