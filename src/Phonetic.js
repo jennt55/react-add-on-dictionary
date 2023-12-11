a1f2f665e20853c0869d29c6d564e861774338bc
jennt55 <146074617+jennt55@users.noreply.github.com>
2023-12-11 11:24:26 +0000

    fixed phonetics

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Phonetic(props) {
-  return <div className="Phonetic">{props.phonetic}</div>;
+  console.log(props.phonetic);
+
+  if (props.phonetic) {
+    return (
+      <div className="phonetics">
+        <h3>{props.phonetic}</h3>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
--- src/Results.js
+++ src/Results.js
@@ -6,16 +6,10 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
-        <div className="PhoneticsResults">
-          {props.results.phonetic.map(function (phonetic, index) {
-            return (
-              <div key={index}>
-                <Phonetic phonetic={phonetic} />
-              </div>
-            );
-          })}
-        </div>
+        <section className="WordPhonetic">
+          <h2>{props.results.word}</h2>
+          <Phonetic phonetic={props.results.phonetic} />
+        </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
